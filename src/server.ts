bd403329428e0d34b169b546d9855a312c7969b3
Mário Fernandes <mariobmf02@gmail.com>
2020-06-04 18:52:49 -0300

    Added ESLint and Prettier, Fixed style of server.ts file

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const app = express();
 
-app.get('/',(request, response)=>{
+app.get('/', (request, response) => {
   return response.send('Initial Commit');
 });
 
